73c46ff39d264d931427e531abf4a2a2b2b6d127
Liam Ansoumane Kadher Kande <liamkande@icloud.com>
2023-06-13 19:35:02 -0700

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Ansoumane Kadher Kande (Liam) on 6/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
